86569d4b90f57ba50ffcb2db37691c3cf0d87d3d
Domy15 <domenico2016dellaventura@gmail.com>
2024-11-29 18:36:27 +0100

    commit

--- base_di_partenza/assets/js/script.js
+++ base_di_partenza/assets/js/script.js
@@ -4,11 +4,38 @@ const btnStart = document.getElementById("btnStart");
 window.addEventListener("scroll", scroll);
 
 function scroll() {
-    if(window.scrollY > 700){
+    if (window.scrollY > 700) {
         header.classList.add("white");
         btnStart.id = "green";
-    }else if (window.scrollY < 700){
+    } else if (window.scrollY < 700) {
         header.classList.remove("white");
         btnStart.id = "btnStart";
     }
+}
+
+function randomLetter() {
+    const letters = document.querySelectorAll("g[stroke-linecap=butt]");
+    let random = Math.floor(Math.random() * letters.length);
+    let currentLetter = letters[random];
+    return currentLetter;
+}
+
+function changeOpacity() {
+    let letter = randomLetter();
+    let opacity = parseInt(letter.getAttribute("opacity"));
+    if (opacity === 0) {
+        letter.setAttribute("opacity", 1);
+    } else {
+        letter.setAttribute("opacity", 0);
+    }
+}
+
+function disappearLetter() {
+    setInterval(function () {
+        changeOpacity();
+    }, 100);
+}
+
+window.onload = function () {
+    disappearLetter();
 }
