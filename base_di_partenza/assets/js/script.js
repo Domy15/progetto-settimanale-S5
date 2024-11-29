a8a10cf05db5bd6bac4125c6e58094ac329b227d
Domy15 <domenico2016dellaventura@gmail.com>
2024-11-29 18:11:11 +0100

    commit

--- base_di_partenza/assets/js/script.js
+++ base_di_partenza/assets/js/script.js
@@ -1,11 +1,14 @@
-const header = document.getElementById("header");
+const header = document.querySelector("header");
+const btnStart = document.getElementById("btnStart");
 
 window.addEventListener("scroll", scroll);
 
 function scroll() {
     if(window.scrollY > 700){
         header.classList.add("white");
+        btnStart.id = "green";
     }else if (window.scrollY < 700){
         header.classList.remove("white");
+        btnStart.id = "btnStart";
     }
 }
